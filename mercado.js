9408154bf5084245fbccf8b2ebc1733c40498036
Andreas Toullon <andreasgitinit@gmail.com>
2025-02-14 23:12:27 -0300

    cadastro dos produtos

---
+++ mercado.js
@@ -0,0 +1,14 @@
+let items = [];
+
+
+document.querySelector('input[type=submit]').addEventListener('click', () => {
+    var nomeProduto = document.querySelector('input[name=nome_produto]').value;
+    var precoProduto = document.querySelector('input[name=valor_produto]').value;
+   
+    items.push({
+        nome: nomeProduto,
+        valor: precoProduto
+    });
+
+    alert(items[0].nome);
+});
